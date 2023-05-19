0a0b3819c77e27db3f553fb9c35c56c6c1a3486a
Sam Bithrey <sambithrey@gmail.com>
2023-05-19 13:40:49 +0100

    Favorite functionality

---
+++ src/providers/ToasterProvider.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Toaster } from "react-hot-toast";
+
+type Props = {};
+
+function ToasterProvider({}: Props) {
+  return <Toaster />;
+}
+
+export default ToasterProvider;
